bae99e76d86abd171c4a24af0cb385a00e2d1864
Manoj Reddy <kakimanojreddy22@gmail.com>
2024-07-26 16:16:37 +0530

    html,css,sandbox.ts/js files are added

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var character = "manoj";
+console.log(character);
+var input = document.querySelectorAll("input");
+console.log(input);
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+const character  = "manoj" ;
+console.log(character)
+const input = document.querySelectorAll("input");
+
+console.log(input)
